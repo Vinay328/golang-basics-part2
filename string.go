95660147c4b67081a7c703fcf1282628941b2049
vinaykoti <vinay.koti@colortokens.com>
2020-07-27 20:42:06 +0530

    understanding string

---
+++ string.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main()  {
+
+	var singleChar = 'i'
+	fmt.Println(singleChar, "and data type is", reflect.TypeOf(singleChar))
+
+	var name = "vinay"
+	fmt.Println(name, "and data type is", reflect.TypeOf(name))
+
+}
